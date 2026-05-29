867ecb0da5bc16a97b00f7d21dceac6d08cd7d6e
Ivo-Olivares <olivares.ivan.gabriel@gmail.com>
2026-05-29 00:44:07 -0300

    Base de datos eCommerce modelada en SQL

--- eCommerce.Dominio/Usuario.cs
+++ eCommerce.Dominio/Usuario.cs
@@ -15,7 +15,7 @@ namespace eCommerce.Dominio
         public DateTime FechaNacimiento { get; set; }
         public string Email { get; set; }
         public string Telefono { get; set; }
-        public string Password { get; set; }
+        public string Clave { get; set; }
         public string Rol { get; set; }
         public bool Activo { get; set; }
         public List<Direccion> ListaDirecciones { get; set; }
