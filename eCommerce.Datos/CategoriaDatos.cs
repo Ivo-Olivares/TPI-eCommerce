93d1b1f3e852e68cc06ab3918732ee19dd4a7c09
Ivo-Olivares <olivares.ivan.gabriel@gmail.com>
2026-05-31 00:54:52 -0300

    Clase CategoriaDatos implementada

---
+++ eCommerce.Datos/CategoriaDatos.cs
@@ -0,0 +1,43 @@
+﻿using eCommerce.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Datos
+{
+    public class CategoriaDatos
+    {
+        public List<Categoria> ListarCategorias()
+        {
+            List<Categoria> lista = new List<Categoria>();
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("SELECT IdCategoria, Nombre FROM CATEGORIAS");
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Categoria categoria = new Categoria();
+                    categoria.Id = (int)datos.Lector["IdCategoria"];
+                    categoria.Nombre = (string)datos.Lector["Nombre"];
+
+                    lista.Add(categoria);
+                }
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
+    }
+}
