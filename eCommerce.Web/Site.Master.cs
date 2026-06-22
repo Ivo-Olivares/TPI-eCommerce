b450abd3e4cff2f9857df661c4ac74d427dc280e
Lucas Alejo Bellesi <alejobellesi@hotmail.com>
2026-06-22 17:39:25 -0300

    Actualiza navegacion segun sesion

--- eCommerce.Web/Site.Master.cs
+++ eCommerce.Web/Site.Master.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using eCommerce.Dominio;
 
 namespace eCommerce.Web
 {
@@ -11,7 +12,41 @@ namespace eCommerce.Web
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            ConfigurarNavegacion();
+        }
 
+        protected void btnCerrarSesion_Click(object sender, EventArgs e)
+        {
+            Session.Clear();
+            Response.Redirect("~/Default.aspx", false);
+        }
+
+        private void ConfigurarNavegacion()
+        {
+            Usuario usuario = Session["Usuario"] as Usuario;
+            bool hayUsuario = usuario != null;
+            bool esInvitado = Session["EsInvitado"] is bool && (bool)Session["EsInvitado"];
+
+            liIngresar.Visible = !hayUsuario;
+            liRegistrarse.Visible = !hayUsuario;
+            liMiPerfil.Visible = hayUsuario && !esInvitado;
+            liMisCompras.Visible = hayUsuario && !esInvitado;
+            liAdmin.Visible = TieneRol(usuario, "Admin") || TieneRol(usuario, "Vendedor");
+            liCerrarSesion.Visible = hayUsuario;
+        }
+
+        private bool TieneRol(Usuario usuario, string nombreRol)
+        {
+            if (usuario == null || usuario.Roles == null)
+                return false;
+
+            foreach (Rol rol in usuario.Roles)
+            {
+                if (string.Equals(rol.Nombre, nombreRol, StringComparison.InvariantCultureIgnoreCase))
+                    return true;
+            }
+
+            return false;
         }
     }
 }
--- eCommerce.Web/Site.Master.designer.cs
+++ eCommerce.Web/Site.Master.designer.cs
@@ -13,6 +13,13 @@ namespace eCommerce.Web
 
     public partial class SiteMaster
     {
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl liIngresar;
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl liRegistrarse;
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl liMiPerfil;
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl liMisCompras;
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl liAdmin;
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl liCerrarSesion;
+        protected global::System.Web.UI.WebControls.LinkButton btnCerrarSesion;
 
         /// <summary>
         /// Control MainContent.
