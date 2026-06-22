7ae18d254a38d651a85b0da3067693e23a675e74
Lucas Alejo Bellesi <alejobellesi@hotmail.com>
2026-06-22 17:37:53 -0300

    Implementa registro de clientes

--- eCommerce.Web/Registro.aspx.cs
+++ eCommerce.Web/Registro.aspx.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Web.UI;
+using eCommerce.Dominio;
+using eCommerce.Negocio;
 
 namespace eCommerce.Web
 {
@@ -7,6 +9,69 @@ namespace eCommerce.Web
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+        }
+
+        protected void btnRegistrarse_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Usuario usuario = CrearUsuarioDesdeFormulario();
+                Direccion direccion = CrearDireccionDesdeFormulario();
+
+                UsuarioNegocio negocio = new UsuarioNegocio();
+                Usuario usuarioRegistrado = negocio.RegistrarCliente(usuario, direccion, txtConfirmarClave.Text);
+
+                Session["Usuario"] = usuarioRegistrado;
+                Session["EsInvitado"] = false;
+
+                Response.Redirect("~/Default.aspx", false);
+            }
+            catch (Exception ex)
+            {
+                MostrarError(ex.Message);
+            }
+        }
+
+        private Usuario CrearUsuarioDesdeFormulario()
+        {
+            Usuario usuario = new Usuario();
+            usuario.Nombre = txtNombre.Text;
+            usuario.Apellido = txtApellido.Text;
+            usuario.Dni = txtDni.Text;
+            usuario.Email = txtEmail.Text;
+            usuario.Telefono = txtTelefono.Text;
+            usuario.Clave = txtClave.Text;
+
+            DateTime fechaNacimiento;
+            if (DateTime.TryParse(txtFechaNacimiento.Text, out fechaNacimiento))
+                usuario.FechaNacimiento = fechaNacimiento;
+
+            return usuario;
+        }
+
+        private Direccion CrearDireccionDesdeFormulario()
+        {
+            Direccion direccion = new Direccion();
+            direccion.Calle = txtCalle.Text;
+            direccion.Localidad = txtLocalidad.Text;
+            direccion.Provincia = txtProvincia.Text;
+            direccion.Observaciones = txtObservaciones.Text;
+
+            int altura;
+            if (int.TryParse(txtAltura.Text, out altura))
+                direccion.Altura = altura;
+
+            int cp;
+            if (int.TryParse(txtCp.Text, out cp))
+                direccion.Cp = cp;
+
+            return direccion;
+        }
+
+        private void MostrarError(string mensaje)
+        {
+            lblError.Text = mensaje;
+            lblError.Visible = true;
         }
     }
 }
--- eCommerce.Web/Registro.aspx.designer.cs
+++ eCommerce.Web/Registro.aspx.designer.cs
@@ -4,6 +4,8 @@ namespace eCommerce.Web
     {
         protected global::System.Web.UI.WebControls.TextBox txtNombre;
         protected global::System.Web.UI.WebControls.TextBox txtApellido;
+        protected global::System.Web.UI.WebControls.TextBox txtDni;
+        protected global::System.Web.UI.WebControls.TextBox txtFechaNacimiento;
         protected global::System.Web.UI.WebControls.TextBox txtEmail;
         protected global::System.Web.UI.WebControls.TextBox txtTelefono;
         protected global::System.Web.UI.WebControls.TextBox txtClave;
@@ -14,6 +16,7 @@ namespace eCommerce.Web
         protected global::System.Web.UI.WebControls.TextBox txtProvincia;
         protected global::System.Web.UI.WebControls.TextBox txtCp;
         protected global::System.Web.UI.WebControls.TextBox txtObservaciones;
+        protected global::System.Web.UI.WebControls.Label lblError;
         protected global::System.Web.UI.WebControls.Button btnRegistrarse;
     }
 }
