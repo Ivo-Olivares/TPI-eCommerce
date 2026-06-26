e6f877e89300ffd3830e397e386798044f60072b
Ivo-Olivares <olivares.ivan.gabriel@gmail.com>
2026-06-26 00:41:40 -0300

    Agrega datos y negocio para detalle de pedido

---
+++ eCommerce.Datos/DetallePedidoDatos.cs
@@ -0,0 +1,34 @@
+﻿using eCommerce.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Datos
+{
+    public class DetallePedidoDatos
+    {
+        public void AgregarDetallePedido(DetallePedido detalle)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.setearConsulta("INSERT INTO DETALLESPEDIDO (IdPedido, IdProducto, Cantidad, PrecioUnitario, Subtotal) VALUES (@IdPedido, @IdProducto, @Cantidad, @PrecioUnitario, @Subtotal)");
+
+                datos.setearParametros("@IdPedido", detalle.Pedido.Id);
+                datos.setearParametros("@IdProducto", detalle.Producto.Id);
+                datos.setearParametros("@Cantidad", detalle.Cantidad);
+                datos.setearParametros("@PrecioUnitario", detalle.PrecioUnitario);
+                datos.setearParametros("@Subtotal", detalle.Subtotal);
+
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ eCommerce.Negocio/DetallePedidoNegocio.cs
@@ -0,0 +1,36 @@
+﻿using eCommerce.Datos;
+using eCommerce.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerce.Negocio
+{
+    public class DetallePedidoNegocio
+    {
+        public void AgregarDetallePedido(DetallePedido detalle)
+        {
+            if (detalle == null)
+                throw new Exception("El detalle del pedido no puede estar vacío.");
+
+            if (detalle.Pedido == null || detalle.Pedido.Id <= 0)
+                throw new Exception("El detalle debe estar asociado a un pedido válido.");
+
+            if (detalle.Producto == null || detalle.Producto.Id <= 0)
+                throw new Exception("El detalle debe tener un producto válido.");
+
+            if (detalle.Cantidad <= 0)
+                throw new Exception("La cantidad debe ser mayor a cero.");
+
+            if (detalle.PrecioUnitario <= 0)
+                throw new Exception("El precio unitario debe ser mayor a cero.");
+
+            detalle.Subtotal = detalle.Cantidad * detalle.PrecioUnitario;
+
+            DetallePedidoDatos datos = new DetallePedidoDatos();
+            datos.AgregarDetallePedido(detalle);
+        }
+    }
+}
