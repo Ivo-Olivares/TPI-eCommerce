4be6a8ebe734ef036fa73508c4c52dacd8f526d5
Lucas Alejo Bellesi <alejobellesi@hotmail.com>
2026-06-26 19:05:05 -0300

    Implementar detalle de producto real

--- eCommerce.Web/DetalleProducto.aspx.designer.cs
+++ eCommerce.Web/DetalleProducto.aspx.designer.cs
@@ -2,6 +2,17 @@ namespace eCommerce.Web
 {
     public partial class DetalleProducto
     {
+        protected global::System.Web.UI.WebControls.Panel pnlMensaje;
+        protected global::System.Web.UI.WebControls.Label lblMensaje;
+        protected global::System.Web.UI.WebControls.Panel pnlDetalle;
+        protected global::System.Web.UI.WebControls.Label lblNombre;
+        protected global::System.Web.UI.WebControls.Label lblMeta;
+        protected global::System.Web.UI.WebControls.Label lblDescripcion;
+        protected global::System.Web.UI.WebControls.Label lblPrecio;
+        protected global::System.Web.UI.WebControls.Label lblStock;
+        protected global::System.Web.UI.WebControls.Label lblEstado;
+        protected global::System.Web.UI.WebControls.Label lblError;
+        protected global::System.Web.UI.WebControls.Label lblExito;
         protected global::System.Web.UI.WebControls.TextBox txtCantidad;
         protected global::System.Web.UI.WebControls.Button btnAgregarCarrito;
     }
